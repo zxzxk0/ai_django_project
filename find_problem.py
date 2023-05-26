5ed5504b4ea44c233abad337383253e0d61ababd
zxzxk0 <51022397+zxzxk0@users.noreply.github.com>
2023-05-26 16:39:12 +0900

    Add files via upload

--- find_problem.py
+++ find_problem.py
@@ -1,18 +1,34 @@
 from konlpy.tag import Kkma
 from konlpy.utils import pprint
 import numpy as np
-def text_split(sentence):
+def text_split1(sentence):
+    kkma = Kkma()
+    return (kkma.morphs(sentence))
+
+def text_split2(sentence):
     kkma = Kkma()
     return (kkma.nouns(sentence))
 
+def text_split3(sentence):
+    kkma = Kkma()
+    return (kkma.pos(sentence))
+
+
 def find_problem(text):
+    # text1=""
+    # text2=""
+    # text3=""
+    # text1 = text_split1(text)
+    # text2= text_split2(text)
+    # text3= text_split3(text)
+    # text = text1+text2+text3
     problem = ["default","love","relation","health_","study","wealth","job"]
     problem_list = [0,0,0,0,0,0,0]
     default = ["몰라","없어"]
-    love = ["연애","남자친구","여자친구","사랑","남친","여친","자기","썸","영화","밥","남사친","여사친","재회","새로운","이별","헤어짐","바람"]
+    love = ["연애","남자친구","여자친구","사랑","남친","여친","자기","썸","영화","밥","남사친","여사친","재회","새로운","이별","헤어짐","바람","연애","남자","여자"]
     relation = ["친구","만남","주변인","지인","사람","손절","주변 사람","친우","대화","학교","연락","대학","동네친구","배려","예의","선","베프","친한","인스타","카톡"]
-    health = ["건강","병원","만수무강","질병","부상","회복","나이","피곤해","졸려","잠","지쳤다","눈","피곤","피곤하다","졸립다","잠온다"]
-    study = ["공부","학업","문제","등수","전교","등급","과목","학점","머리","두뇌","수학","영어","국어","필기","시험","장학금","입시"]
+    health = ["건강","병원","만수무강","질병","부상","회복","나이","피곤해","졸려","잠","지쳤다","눈","피곤","피곤하다","졸립다","잠온다","졸리"]
+    study = ["공부","학업","문제","등수","전교","등급","과목","학점","머리","두뇌","수학","영어","국어","필기","시험","장학금","입시","졸전","전시회","졸업"]
     wealth = ["돈","머니","형편","재산","부동산","지갑","지폐","가사","대박","부도","동전","저금","저축"]
     job = ["직장","공채","취준","취업","취직","직업","직종","채용","면접","자소서","기업","회사","진로","꿈","야망","하다"]
     for item in text:
@@ -30,6 +46,7 @@ def find_problem(text):
             problem_list[5]=problem_list[5]+1
         if any(str in item for str in job):
             problem_list[6]=problem_list[6]+1   
+    
 
     return problem[np.argmax(problem_list)]
 
